b7939fa732317661fa082a20bb5aff5e61ab4861
Pontus Asp <pontus.asp@gmail.com>
2020-10-26 07:53:05 +0100

    made homepage

---
+++ script-landing.js
@@ -0,0 +1,13 @@
+function coinClick() {
+    var elem = document.getElementById('content');
+    var cf = document.getElementById('coin-front');
+    var cb = document.getElementById('coin-back');
+
+    if(elem.classList.contains('flip')) {
+        window.location.href = 'home.html';
+    } else {
+        elem.classList.add('flip');
+        cf.classList.add('back');
+        cb.classList.add('front');
+    }
+}
